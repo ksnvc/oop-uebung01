9ec3dd722f4b1f11406e59fcd361fe43d9db5777
Sara Kosanovic <sarakosanovic@Saras-Air.dhcp.fh-rosenheim.de>
2019-03-26 10:42:18 +0100

    Loesung

--- src/main/java/ueb01/CharStackImpl.java
+++ src/main/java/ueb01/CharStackImpl.java
@@ -1,4 +1,42 @@
 package ueb01;
 
+import java.util.NoSuchElementException;
+
 class CharStackImpl implements CharStack {
+
+   private char[]z;
+
+    public CharStackImpl(){
+    z = new char[0];
+    }
+
+    @Override
+    public void push(char c) {
+        char[]p = new char[z.length + 1];
+        for(int i = 0; i<z.length; i++){
+            p[i] = z[i];
+        }
+        p[z.length] = c;
+        z = p;
+    }
+
+    @Override
+    public char pop() {
+        if(z.length == 0) {
+            throw new NoSuchElementException();
+        }
+        char a = z[z.length-1];
+        char[]p = new char[z.length-1];
+        for(int i = 0; i<z.length-1; i++)
+        { p[i] = z[i];
+        }
+        z =p;
+        return a;
+    }
+
+    @Override
+    public int size() {
+        return z.length;
+    }
+
 }
