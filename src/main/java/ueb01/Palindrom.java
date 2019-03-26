4aa5e69fbfb6b40db3f22ee2629baf21e0e12865
Sara Kosanovic <sarakosanovic@Saras-Air.dhcp.fh-rosenheim.de>
2019-03-26 11:17:13 +0100

    PalindromLoesung

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -2,8 +2,16 @@ package ueb01;
 
 class Palindrom {
 	static boolean istPalindrom(String s) {
-		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen!
+		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen! = faboolean pali;
+		boolean pali = true;
+		s=s.replaceAll(" ", "");
+		s=s.toLowerCase();
+		char[]z =s.toCharArray();
 
-		return false;
+		for(int i=0; i<s.length()/2; i++){
+			if(!(z[i]==z[z.length-1-i]))
+			{ pali = false;}
+		}
+		return pali;
 	}
 }
