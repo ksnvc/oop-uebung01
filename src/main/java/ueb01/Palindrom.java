bb8ac539865b04e0b42b5b70f01567390e8f3aad
ksnvc <sarakosanovic11@gmail.com>
2019-06-10 20:58:53 +0200

    loesung

--- src/main/java/ueb01/Palindrom.java
+++ src/main/java/ueb01/Palindrom.java
@@ -1,17 +1,24 @@
 package ueb01;
 
 class Palindrom {
-	static boolean istPalindrom(String s) {
-		// Verwenden Sie die Klasse CharStackImpl um auf Palindrom zu testen! = faboolean pali;
-		boolean pali = true;
-		s=s.replaceAll(" ", "");
-		s=s.toLowerCase();
-		char[]z =s.toCharArray();
 
-		for(int i=0; i<s.length()/2; i++){
+	static boolean istPalindrom(String s) {
+
+		if(s.isEmpty())
+			return false;
+
+		if(s.length() == 1)
+			return true;
+		
+
+		char [] z = s.replaceAll(" ", "").toLowerCase().toCharArray();
+
+		for(int i=0; i<z.length/2; i++){
+
 			if(!(z[i]==z[z.length-1-i]))
-			{ pali = false;}
+			 return false;
 		}
-		return pali;
+
+		return true;
 	}
 }
--- src/test/java/ueb01/PalindromTest.java
+++ src/test/java/ueb01/PalindromTest.java
@@ -1,5 +1,6 @@
 package ueb01;
 
+
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
